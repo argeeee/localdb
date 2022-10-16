ec2680ebfaf6042e7ef7dd3052b9eeb9acd45783
Argetim Jusufi <argetimjusufi01@gmail.com>
2022-10-16 11:36:57 +0200

    [Refactor] Some refactoring

--- localdb.py
+++ localdb.py
@@ -6,26 +6,24 @@ import os
 import shutil
 
 from localdb.database import Database
+from localdb.utils import create_folder_if_not_exist
 
 class LocalDB:
 	db_folder: str
 
 	def __init__(self, db_folder: str):
 		self.db_folder = db_folder
-		self._create_folder_if_not_exist(self.db_folder)
+		create_folder_if_not_exist(self.db_folder)
 
 	def load_db(self, database_name: str) -> Database:
-		pass
+		path = os.path.join(self.db_folder, database_name)
+		return Database(database_name, path)
 	
 	def create_db(self, database_name: str):
 		path = os.path.join(self.db_folder, database_name)
-		self._create_folder_if_not_exist(path)
+		create_folder_if_not_exist(path)
 
 	def delete_db(self, database_name: str) -> Database:
 		path = os.path.join(self.db_folder, database_name)
 		shutil.rmtree(path)
 	
-	# Private methods
-	def _create_folder_if_not_exist(self, directory: str):
-		if not os.path.exists(directory):
-			os.makedirs(directory)
